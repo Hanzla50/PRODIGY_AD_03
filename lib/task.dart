6c792dd28fc0e297a939defd52af70de9957d1fa
Hanzla50 <hanzlajaved50281@gmail.com>
2024-06-22 22:51:12 +0500

    frontend

--- lib/task.dart
+++ lib/task.dart
@@ -1,12 +1,100 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
+class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
+
+  @override
+  _MyWidgetState createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  late Stopwatch _stopwatch;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _stopwatch = Stopwatch();
+    _timer = Timer.periodic(const Duration(milliseconds: 30), (timer) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _startStopwatch() {
+    setState(() {
+      _stopwatch.start();
+    });
+  }
+
+  void _stopStopwatch() {
+    setState(() {
+      _stopwatch.stop();
+    });
+  }
+
+  void _resetStopwatch() {
+    setState(() {
+      _stopwatch.reset();
+    });
+  }
+
+  String _formattedTime() {
+    final duration = _stopwatch.elapsed;
+    final minutes = duration.inMinutes.remainder(60).toString().padLeft(2, '0');
+    final seconds = duration.inSeconds.remainder(60).toString().padLeft(2, '0');
+    final milliseconds = (duration.inMilliseconds.remainder(1000) ~/ 10).toString().padLeft(2, '0');
+    return '$minutes:$seconds.$milliseconds';
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
+      appBar: AppBar(
+        title: const Text('Stopwatch'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _formattedTime(),
+              style: const TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _startStopwatch,
+                  child: const Text('Start'),
+                ),
+                const SizedBox(width: 10.0),
+                ElevatedButton(
+                  onPressed: _stopStopwatch,
+                  child: const Text('Stop'),
+                ),
+                const SizedBox(width: 10.0),
+                ElevatedButton(
+                  onPressed: _resetStopwatch,
+                  child: const Text('Reset'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
